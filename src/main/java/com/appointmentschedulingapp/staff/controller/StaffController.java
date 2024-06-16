5fa6acd2706c56865ba1c38da4d13c6bb86f1b50
Ananda Magar <anandakmagar@gmail.com>
2024-06-16 13:54:25 -0600

    first commit

--- src/main/java/com/appointmentschedulingapp/staff/controller/StaffController.java
+++ src/main/java/com/appointmentschedulingapp/staff/controller/StaffController.java
@@ -5,18 +5,15 @@ import com.appointmentschedulingapp.staff.dto.StaffDTO;
 import com.appointmentschedulingapp.staff.service.IStaffService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/staff")
-@CrossOrigin(origins = "https://security-ms-59cfc31741b3.herokuapp.com/")
+@CrossOrigin(origins = {"https://security-ms-59cfc31741b3.herokuapp.com", "https://appointment-app-frontend-086470168ae5.herokuapp.com"})
 public class StaffController {
     @Autowired
     private IStaffService iStaffService;
@@ -85,31 +82,11 @@ public class StaffController {
         return iStaffService.fetchStaffNameByStaffId(staffId);
     }
 
-//    @GetMapping("/fetchAllStaff")
-//    @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
-//    public List<StaffDTO> fetchAllStaff(){
-//        return iStaffService.fetchAllStaff();
-//    }
-
-    @Autowired
-    private WebClient.Builder webClientBuilder;
-
     @GetMapping("/fetchAllStaff")
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
-    public ResponseEntity<List<StaffDTO>> fetchAllStaff(@RequestHeader("Authorization") String authorizationHeader) {
-        WebClient client = webClientBuilder.baseUrl("https://staff-ms-03792ef7327d.herokuapp.com").build();
-        String uri = "/admin-staff/fetchAllStaff";
-
-        // Make a GET request to the sender microservice with Authorization header
-        List<StaffDTO> staffList = client.get()
-                .uri(uri)
-                .header("Authorization", authorizationHeader)
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<StaffDTO>>() {})
-                .block(); // Blocking to retrieve the response synchronously
-
-        // Return the response received from the sender microservice
-        return ResponseEntity.ok(staffList);
+    public List<StaffDTO> fetchAllStaff(){
+        return iStaffService.fetchAllStaff();
     }
+
 }
 
